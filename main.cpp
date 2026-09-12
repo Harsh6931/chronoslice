3fd7ac27166a7cd6559f8927d8e51201c50ba63b
HARSHIT SINHA <harshitsinha2006@gmail.com>
2026-09-12 12:54:22 +0530

    also packed the statistics value in  a struct

--- main.cpp
+++ main.cpp
@@ -13,19 +13,31 @@ struct logEntry {
     long long bandwidth;
     int latency;
 
+
+
 };
-void updateStastics(logEntry &entry,int &code_2xx,int &code_4xx,int &code_5xx,long long &total_bandwith,vector<int> &latencies){
+
+struct statistics {
+    int code_2xx;
+    int code_4xx;
+    int code_5xx;
+    long long total_bandwidth;
+    vector<int> latencies; //store latency
+
+};
+
+void updateStastics(const logEntry &entry, statistics &stats){
     if(entry.status>=200 && entry.status<300){
-        code_2xx++;
+        stats.code_2xx++;
     }
     else if(entry.status>=400 && entry.status<500){
-        code_4xx++;
+        stats.code_4xx++;
     }
     else if(entry.status>=500 && entry.status<600){
-        code_5xx++;
+        stats.code_5xx++;
     }
-    total_bandwith+=entry.bandwidth;
-    latencies.push_back(entry.latency);
+    stats.total_bandwidth+=entry.bandwidth;
+    stats.latencies.push_back(entry.latency);
 
 }
 // using const as string line cant change
@@ -58,27 +70,22 @@ int main()
         cout << "Failed to open log file\n";
         return 1;
     }
-    string line;
-    int code_2xx = 0;
-    int code_4xx = 0;
-    int code_5xx = 0;
-    long long total_bandwith=0;
-    //store latency
-    vector<int> latencies;
-    while (getline(file, line)){
 
+    string line;
+    statistics stats={0,0,0,0,{}};  // default initialization of statistics struct
+
+    while (getline(file, line)){
         logEntry entry = parseLine(line);
-        
-        updateStastics(entry,code_2xx,code_4xx,code_5xx,total_bandwith,latencies);
+        updateStastics(entry,stats);
     }
-    sort(latencies.begin(),latencies.end());
-    cout << "2xx: " << code_2xx << endl;
-    cout << "4xx: " << code_4xx << endl;
-    cout << "5xx: " << code_5xx << endl;
-    cout << "Total Bandwidth: " << total_bandwith << endl;
-    int p95_index=ceil(latencies.size()*0.95);
+    sort(stats.latencies.begin(),stats.latencies.end());
+    cout << "2xx: " << stats.code_2xx << endl;
+    cout << "4xx: " << stats.code_4xx << endl;
+    cout << "5xx: " << stats.code_5xx << endl;
+    cout << "Total Bandwidth: " << stats.total_bandwidth << endl;
+    int p95_index=ceil(stats.latencies.size()*0.95);
     cout<<"P95 index :"<<p95_index<<endl;
-    cout<<"P95 latency :"<<latencies[p95_index-1]<<endl;
+    cout<<"P95 latency :"<<stats.latencies[p95_index-1]<<endl;
 
     return 0;
 }
